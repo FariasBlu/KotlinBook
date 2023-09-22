384921539f37d77a7af3e180275627113a7985c8
Celso Farias <celso.farias@useblu.com.br>
2023-09-22 00:11:00 -0300

    bit a bit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,4 +26,9 @@ fun main() {
         val leftShift = 1 shl 2
         val rightShift = 1 shr 2
         val unsignedShift = 1 ushr 2
+
+        val and = 1 and 0x00001111
+        val or = 1 or 0x00001111
+        val xor = 1 xor 0x00001111
+        val inv = 1.inv()
 }
